add465834eee42bdb4511011196bf5e59cc344b3
Dannette Tejeda <dannettetejeda@gmail.com>
2020-11-10 11:19:08 -0500

    connection addon

--- cli.js
+++ cli.js
@@ -4,7 +4,10 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
 // Connection Set up
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
